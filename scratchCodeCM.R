15a8dcc226f79c30f56f6f03f9bfcf988893f1e0
rubabahasan <rubabahasanb@gmail.com>
2019-04-03 15:55:45 -0400

    added scratchCodeCM.R

--- scratchCodeCM.R
+++ scratchCodeCM.R
@@ -3,8 +3,33 @@ library(randomForest)
 library(gbm)
 setwd("/Users/rxh655/Documents/Spring2019/STAT557/Project2")
 
-mushroom =read.csv("CrowdsourcedMapping/training.csv")
+cmDatatraining =read.csv("CrowdsourcedMapping/training.csv")
+cmDatatest = read.csv("CrowdsourcedMapping/testing.csv")
 
-tree.mushroom = tree(mushroomData$classification~.-mushroomData$classification,mushroomData, subset = train)
-tree.pred=predict(tree.mushroom, mushroomData.test ,type ="class")
+tree.cm = tree(cmDatatraining$class, cmDatatraining)
+tree.pred
+
+classification.test = cmDatatest$class
+
+
+tree.cm = tree(cmDatatraining$class~.-cmDatatraining$class,cmDatatraining)
+tree.pred=predict(tree.cm, cmDatatest ,type ="class")
 table(tree.pred , classification.test)
+
+(36+71+7+30+7)/(301)
+
+set.seed(1)
+bag.sm = randomForest(cmDatatraining$class~.-cmDatatraining$class,data=cmDatatraining,mtry=28,importance=TRUE)
+bag.sm
+
+yhat.bag = predict(bag.sm,newdata=cmDatatest)
+
+yhat.bag
+table(yhat.bag, classification.test)
+(40+67+5+36+23)/301
+
+set.seed(1)
+boost.cm = gbm(cmDatatraining$class~., data = cmDatatraining, distribution = "gaussian",n.trees=5000,interaction.depth=4)
+yhat.boost = predict(boost.cm,newdata=cmDatatest,n.trees=5000)
+table(yhat.boost, classification.test)
+summary(yhat.boost)
