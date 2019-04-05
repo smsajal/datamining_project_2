16830138386fb7c767d8c9a2255a0107f86c617e
rubabahasan <rubabahasanb@gmail.com>
2019-04-05 01:16:57 -0400

    final random forest and boosting code in rf_boosting.R

---
+++ rf_boosting.R
@@ -0,0 +1,275 @@
+library(tree)
+library(randomForest)
+library(gbm)
+library(adabag)
+library(pROC)
+library(ROCR)
+library(car)
+library(dummies)
+
+rm(list=ls())
+
+setwd("/Users/rxh655/Documents/Spring2019/STAT557/Project2/projectCode/datamining_project_2/data")
+
+cmDatatraining =read.csv("training.csv")
+cmDatatest = read.csv("testing.csv")
+classification.test = cmDatatest$class
+
+#### TO SEPARATE TRAINING INTO TEST AND TRAINING
+set.seed(1)
+train=sample(1:nrow(cmDatatraining), .75*nrow(cmDatatraining))
+cmDatatraining = cmDatatraining[train,]
+cmDatatest=cmDatatraining[-train,]
+classification.test = cmDatatest$class
+########### Read data End #############
+
+######### Random Forest #############
+
+rf(cmDatatraining, cmDatatest)
+
+rf <- function(cmDatatraining, cmDatatest)
+{
+  classification.test = cmDatatest$class
+  
+  set.seed(1)
+  s1 = Sys.time()
+  bag.sm = randomForest(cmDatatraining$class~.-cmDatatraining$class, data=cmDatatraining,  importance=TRUE)
+  s2 = Sys.time()
+  trainTime = s2-s1
+  print(paste("Train time ", trainTime))
+  
+  s1 = Sys.time()
+  predict.class = predict(bag.sm,newdata=cmDatatest)
+  s2 = Sys.time()
+  classifyTime = s2-s1
+  print(paste("Classification time", classifyTime))
+  #yhat.bag
+  #table(yhat.bag, classification.test)
+  print(paste('Accuracy', 1-mean(as.character(predict.class) != as.character(classification.test))))
+  print(paste('Error rate: ', mean(as.character(predict.class) != as.character(classification.test))))
+  
+  attach(mtcars)
+  par(mfrow=c(4,3))
+  
+  
+  #ROC for train
+  pred_prob = predict(bag.sm, newdata=cmDatatraining, type = "prob")
+  
+  pred = pred_prob[,1]
+  dummytest <- dummy.data.frame(cmDatatraining)
+  y = dummytest$classfarm
+  roc1 = roc(y, pred)
+  print(paste('AUC: ', auc(roc1)))
+  plot(roc1, col = 1, lty = 2, main = "ROC for Farm Class (Train data)")
+  
+  pred = pred_prob[,2]
+  dummytest <- dummy.data.frame(cmDatatraining)
+  y = dummytest$classforest
+  roc2 = roc(y, pred)
+  print(paste('AUC: ', auc(roc2)))
+  plot(roc2, col = 1, lty = 2, main = "ROC for Forest Class (Train data)")
+  
+  pred = pred_prob[,3]
+  dummytest <- dummy.data.frame(cmDatatraining)
+  y = dummytest$classgrass
+  roc3 = roc(y, pred)
+  print(paste('AUC: ', auc(roc3)))
+  plot(roc3, col = 1, lty = 2, main = "ROC for Grass Class (Train data)")
+  
+  pred = pred_prob[,4]
+  dummytest <- dummy.data.frame(cmDatatraining)
+  y = dummytest$classimpervious
+  roc4 = roc(y, pred)
+  print(paste('AUC: ', auc(roc4)))
+  plot(roc4, col = 1, lty = 2, main = "ROC for Impervious Class (Train data)")
+  
+  pred = pred_prob[,5]
+  dummytest <- dummy.data.frame(cmDatatraining)
+  y = dummytest$classorchard
+  roc5 = roc(y, pred)
+  print(paste('AUC: ', auc(roc5)))
+  plot(roc5, col = 1, lty = 2, main = "ROC for Orchard Class (Train data)")
+  
+  pred = pred_prob[,6]
+  dummytest <- dummy.data.frame(cmDatatraining)
+  y = dummytest$classwater
+  roc6 = roc(y, pred)
+  print(paste('AUC: ', auc(roc6)))
+  plot(roc6, col = 1, lty = 2, main = "ROC for Water Class (Train data)")
+  
+  print(paste("Average AUC ", mean(auc(roc1), auc(roc2), auc(roc3), auc(roc4), auc(roc5), auc(roc6))))
+  
+  #ROC for test
+  pred_prob = predict(bag.sm, newdata=cmDatatest, type = "prob")
+  
+  pred = pred_prob[,1]
+  dummytest <- dummy.data.frame(cmDatatest)
+  y = dummytest$classfarm
+  roc1 = roc(y, pred)
+  print(paste('AUC: ', auc(roc1)))
+  plot(roc1, col = 1, lty = 2, main = "ROC for Farm Class (Test Data)")
+  
+  pred = pred_prob[,2]
+  dummytest <- dummy.data.frame(cmDatatest)
+  y = dummytest$classforest
+  roc2 = roc(y, pred)
+  print(paste('AUC: ', auc(roc2)))
+  plot(roc2, col = 1, lty = 2, main = "ROC for Forest Class (Test Data)")
+
+  pred = pred_prob[,3]
+  dummytest <- dummy.data.frame(cmDatatest)
+  y = dummytest$classgrass
+  roc3 = roc(y, pred)
+  print(paste('AUC: ', auc(roc3)))
+  plot(roc3, col = 1, lty = 2, main = "ROC for Grass Class (Test Data)")
+  
+  pred = pred_prob[,4]
+  dummytest <- dummy.data.frame(cmDatatest)
+  y = dummytest$classimpervious
+  roc4 = roc(y, pred)
+  print(paste('AUC: ', auc(roc4)))
+  plot(roc4, col = 1, lty = 2, main = "ROC for Impervious Class (Test Data)")
+  
+  pred = pred_prob[,5]
+  dummytest <- dummy.data.frame(cmDatatest)
+  y = dummytest$classorchard
+  roc5 = roc(y, pred)
+  print(paste('AUC: ', auc(roc5)))
+  plot(roc5, col = 1, lty = 2, main = "ROC for Orchard Class (Test Data)")
+  
+  pred = pred_prob[,6]
+  dummytest <- dummy.data.frame(cmDatatest)
+  y = dummytest$classwater
+  roc6 = roc(y, pred)
+  print(paste('AUC: ', auc(roc6)))
+  plot(roc6, col = 1, lty = 2, main = "ROC for Water Class (Test Data)")
+  
+  print(paste("Average AUC ", mean(auc(roc1), auc(roc2), auc(roc3), auc(roc4), auc(roc5), auc(roc6))))
+  
+  
+}
+
+######### Random Forest End #############
+
+############# Boosting ##################
+
+drawROCboost(trainingData = cmDatatraining, cmDatatest)
+
+drawROCboost <- function(trainingData, testData)
+{
+  classification.test = cmDatatest$class
+  
+  s1 = Sys.time()
+  adaboost = boosting(class~., data = trainingData, mfinal = 500)
+  s2 = Sys.time()
+  trainTime = s2-s1
+  print(paste("Train time ", trainTime))
+  
+  s1 = Sys.time()
+  pred.boost = predict.boosting(adaboost, newdata = testData, type = "prob")
+  s2 = Sys.time()
+  classifyTime = s2-s1
+  print(paste("Classification time", classifyTime))
+  
+  print(paste('Accuracy',1-mean(as.character(pred.boost$class) != as.character(classification.test))))
+  print(paste("Error rate: ", mean(as.character(pred.boost$class) != as.character(classification.test))))
+  
+  
+  attach(mtcars)
+  par(mfrow=c(4,3))
+  
+  #ROC train
+  pred.boost = predict.boosting(adaboost, newdata = trainingData, type = "prob")
+  
+  pred = (pred.boost$prob)[,1]
+  dummytest <- dummy.data.frame(trainingData)
+  y = dummytest$classfarm
+  roc1 = roc(y, pred)
+  print(paste('AUC: ', auc(roc1)))
+  plot(roc1, col = 1, lty = 2, main = "ROC for Farm Class (Train data)")
+  
+  pred = (pred.boost$prob)[,2]
+  dummytest <- dummy.data.frame(trainingData)
+  y = dummytest$classforest
+  roc2 = roc(y, pred)
+  print(paste('AUC: ', auc(roc2)))
+  plot(roc2, col = 1, lty = 2, main = "ROC for Forest Class (Train data)")
+  
+  pred = (pred.boost$prob)[,3]
+  dummytest <- dummy.data.frame(trainingData)
+  y = dummytest$classgrass
+  roc3 = roc(y, pred)
+  print(paste('AUC: ', auc(roc3)))
+  plot(roc3, col = 1, lty = 2, main = "ROC for Grass Class (Train data)")
+  
+  pred = (pred.boost$prob)[,4]
+  dummytest <- dummy.data.frame(trainingData)
+  y = dummytest$classimpervious
+  roc4 = roc(y, pred)
+  print(paste('AUC: ', auc(roc4)))
+  plot(roc4, col = 1, lty = 2, main = "ROC for Impervious Class (Train data)")
+  
+  pred = (pred.boost$prob)[,5]
+  dummytest <- dummy.data.frame(trainingData)
+  y = dummytest$classorchard
+  roc5 = roc(y, pred)
+  print(paste('AUC: ', auc(roc5)))
+  plot(roc5, col = 1, lty = 2, main = "ROC for Orchard Class (Train data)")
+  
+  pred = (pred.boost$prob)[,6]
+  dummytest <- dummy.data.frame(trainingData)
+  y = dummytest$classwater
+  roc6 = roc(y, pred)
+  print(paste('AUC: ', auc(roc6)))
+  plot(roc6, col = 1, lty = 2, main = "ROC for Water Class (Train data)")
+  
+  print(paste("Average AUC ", mean(auc(roc1), auc(roc2), auc(roc3), auc(roc4), auc(roc5), auc(roc6))))
+  
+  #ROC test
+  
+  pred.boost = predict.boosting(adaboost, newdata = testData, type = "prob")
+  
+  pred = (pred.boost$prob)[,1]
+  dummytest <- dummy.data.frame(testData)
+  y = dummytest$classfarm
+  roc1 = roc(y, pred)
+  print(paste('AUC: ', auc(roc1)))
+  plot(roc1, col = 1, lty = 2, main = "ROC for Farm Class (Test data)")
+  
+  pred = (pred.boost$prob)[,2]
+  dummytest <- dummy.data.frame(testData)
+  y = dummytest$classforest
+  roc2 = roc(y, pred)
+  print(paste('AUC: ', auc(roc2)))
+  plot(roc2, col = 1, lty = 2, main = "ROC for Forest Class (Test data)")
+  
+  pred = (pred.boost$prob)[,3]
+  dummytest <- dummy.data.frame(testData)
+  y = dummytest$classgrass
+  roc3 = roc(y, pred)
+  print(paste('AUC: ', auc(roc3)))
+  plot(roc3, col = 1, lty = 2, main = "ROC for Grass Class (Test data)")
+  
+  pred = (pred.boost$prob)[,4]
+  dummytest <- dummy.data.frame(testData)
+  y = dummytest$classimpervious
+  roc4 = roc(y, pred)
+  print(paste('AUC: ', auc(roc4)))
+  plot(roc4, col = 1, lty = 2, main = "ROC for Impervious Class (Test data)")
+  
+  pred = (pred.boost$prob)[,5]
+  dummytest <- dummy.data.frame(testData)
+  y = dummytest$classorchard
+  roc5 = roc(y, pred)
+  print(paste('AUC: ', auc(roc5)))
+  plot(roc5, col = 1, lty = 2, main = "ROC for Orchard Class (Test data)")
+  
+  pred = (pred.boost$prob)[,6]
+  dummytest <- dummy.data.frame(testData)
+  y = dummytest$classwater
+  roc6 = roc(y, pred)
+  print(paste('AUC: ', auc(roc6)))
+  plot(roc6, col = 1, lty = 2, main = "ROC for Water Class (Test data)")
+  
+  print(paste("Average AUC ", mean(auc(roc1), auc(roc2), auc(roc3), auc(roc4), auc(roc5), auc(roc6))))
+}
--- scratchCodeCM.R
+++ scratchCodeCM.R
@@ -1,35 +1,171 @@
 library(tree)
 library(randomForest)
 library(gbm)
-setwd("/Users/rxh655/Documents/Spring2019/STAT557/Project2")
+library(adabag)
+library(pROC)
+library(ROCR)
+library(car)
+library(caret)
+setwd("/Users/rxh655/Documents/Spring2019/STAT557/Project2/projectCode/datamining_project_2/data")
 
-cmDatatraining =read.csv("CrowdsourcedMapping/training.csv")
-cmDatatest = read.csv("CrowdsourcedMapping/testing.csv")
-
-tree.cm = tree(cmDatatraining$class, cmDatatraining)
-tree.pred
-
+cmDatatraining =read.csv("training.csv")
+cmDatatest = read.csv("testing.csv")
 classification.test = cmDatatest$class
 
+set.seed(1)
+train=sample(1:nrow(cmDatatraining), .75*nrow(cmDatatraining))
+test=cmDatatraining[-train,]
+test.class=test$class
+########### Read data End #############
 
+#system.time(tree(cmDatatraining$class~.-cmDatatraining$class,cmDatatraining))
+s1 <- Sys.time()
 tree.cm = tree(cmDatatraining$class~.-cmDatatraining$class,cmDatatraining)
+s2 <- Sys.time()
+s2-s1
 tree.pred=predict(tree.cm, cmDatatest ,type ="class")
 table(tree.pred , classification.test)
 
 (36+71+7+30+7)/(301)
 
+######### Random Forest #############
 set.seed(1)
-bag.sm = randomForest(cmDatatraining$class~.-cmDatatraining$class,data=cmDatatraining,mtry=28,importance=TRUE)
+s1 = Sys.time()
+bag.sm = randomForest(cmDatatraining$class~.-cmDatatraining$class,data=cmDatatraining,  importance=TRUE)
+s2 = Sys.time()
+trainTime = s2-s1
+trainTime
 bag.sm
 
+s1 = Sys.time()
 yhat.bag = predict(bag.sm,newdata=cmDatatest)
-
+s2 = Sys.time()
+classifyTime = s2-s1
 yhat.bag
 table(yhat.bag, classification.test)
+classifyTime
+
+p = predict(bag.sm, newdata = cmDatatest, type = 'prob')
+write.csv(p, file = "predictedVal.csv")
+write.csv(classification.test, file = "labels.csv")
+
+ROC_rf = multiclass.roc(classification.test, predict(bag.sm, newdata = cmDatatest, type = 'prob'))
+ROC_rf$rocs$`farm/forest`
+ROC_rf
+
+?randomForest
+attach(mtcars)
+par(mfrow=c(4,2))
+plot(ROC_rf$rocs$`farm/forest`[[1]], main = "farm/forest")
+plot(ROC_rf$rocs$`farm/forest`[[2]], main = "farm/forest")
+plot(ROC_rf$rocs$`farm/grass`[[1]], main = "farm/grass")
+plot(ROC_rf$rocs$`farm/grass`[[2]], main = "farm/grass")
+plot(ROC_rf$rocs$`farm/impervious`[[1]], main = "farm/impervious")
+plot(ROC_rf$rocs$`farm/impervious`[[2]], main = "farm/impervious")
+plot(ROC_rf$rocs$`farm/orchard`[[1]], main = "farm/orchard")
+plot(ROC_rf$rocs$`farm/orchard`[[2]], main = "farm/orchard")
+
+plot(ROC_rf$rocs$`farm/water`[[1]], main = "farm/water")
+plot(ROC_rf$rocs$`farm/water`[[2]], main = "farm/water")
+plot(ROC_rf$rocs$`forest/grass`[[1]], main = "forest/grass")
+plot(ROC_rf$rocs$`forest/grass`[[2]], main = "forest/grass")
+plot(ROC_rf$rocs$`forest/impervious`[[1]], main = "forest/impervious")
+plot(ROC_rf$rocs$`forest/impervious`[[2]], main = "forest/impervious")
+plot(ROC_rf$rocs$`forest/orchard`[[1]], main = "forest/orchard")
+plot(ROC_rf$rocs$`forest/orchard`[[2]], main = "forest/orchard")
+
+plot(ROC_rf$rocs$`forest/water`[[1]], main = "forest/water")
+plot(ROC_rf$rocs$`forest/water`[[2]], main = "forest/water")
+plot(ROC_rf$rocs$`grass/impervious`[[1]], main = "grass/impervious")
+plot(ROC_rf$rocs$`grass/impervious`[[2]], main = "grass/impervious")
+plot(ROC_rf$rocs$`grass/orchard`[[1]], main = "grass/orchard")
+plot(ROC_rf$rocs$`grass/orchard`[[2]], main = "grass/orchard")
+plot(ROC_rf$rocs$`grass/water`[[1]], main = "grass/water")
+plot(ROC_rf$rocs$`grass/water`[[2]], main = "grass/water")
+
+plot(ROC_rf$rocs$`impervious/orchard`[[1]], main = "impervious/orchard")
+plot(ROC_rf$rocs$`impervious/orchard`[[2]], main = "impervious/orchard")
+plot(ROC_rf$rocs$`impervious/water`[[1]], main = "impervious/water")
+plot(ROC_rf$rocs$`impervious/water`[[2]], main = "impervious/water")
+plot(ROC_rf$rocs$`orchard/water`[[1]], main = "orchard/water")
+plot(ROC_rf$rocs$`orchard/water`[[2]], main = "orchard/water")
+
+
+(301-(40+70+7+38+37))/301
+
+### Using 75-25 on training data #####
+s1 = Sys.time()
+bag.sm = randomForest(cmDatatraining$class~.-cmDatatraining$class,data=cmDatatraining, subset = train, importance=TRUE)
+s2 = Sys.time()
+trainTime = s2-s1
+trainTime
+
+s1 = Sys.time()
+yhat.bag = predict(bag.sm, newdata = test)
+s2 = Sys.time()
+classifyTime = s2-s1
+classifyTime
+table(yhat.bag, test.class)
+
+yhat.bag = predict(bag.sm, newdata = test, type = "prob")
+yhat.bag
+dim(yhat.bag)
+length(test.class)
+
+ROC_rf = multiclass.roc(test.class, predict(bag.sm, newdata = test, type = 'prob'))
+a = predict(bag.sm, newdata = test, type = 'prob')
+a
+ROC_rf$rocs$`farm/forest`
+plot(ROC_rf$rocs$`farm/forest`[[2]])
+
+
+########### Random Forest End ###########
+
 (40+67+5+36+23)/301
+(44+65+7+35+22)/301
+(288+1842+90+215+1+37)/(.25*nrow(cmDatatraining))
 
 set.seed(1)
-boost.cm = gbm(cmDatatraining$class~., data = cmDatatraining, distribution = "gaussian",n.trees=5000,interaction.depth=4)
+boost.cm = gbm(cmDatatraining$class~., data = cmDatatraining, distribution = "bernoulli",n.trees=500,interaction.depth=4)
 yhat.boost = predict(boost.cm,newdata=cmDatatest,n.trees=5000)
 table(yhat.boost, classification.test)
 summary(yhat.boost)
+
+#### Using 75-25 on training data #####
+set.seed(1)
+boost.cm = gbm(cmDatatraining[train,]$class~., data = cmDatatraining[train,], distribution = "multinomial",interaction.depth=4)
+yhat.boost = predict(boost.cm,newdata=test,n.trees=5000, type = "prob")
+
+yhat.boost
+table(yhat.boost, test.class)
+summary(yhat.boost)
+
+?predict
+
+adaboost = boosting(class~., data = cmDatatraining[train,], mfinal = 200)
+pred.boost = predict.boosting(adaboost,newdata=cmDatatraining[-train, ], type = "prob")
+is.matrix(pred.boost$prob)
+dim(pred.boost$prob)
+length(test.class)
+
+
+pr = predict(adaboost, newdata = cmDatatraining[-train,], type = "prob")
+pr
+pr = rbind(c("farm", "forest", "grass", "impervious", "orchard", "water"), pr$prob)
+ROC_rf = multiclass.roc(test.class, pr)
+####boost roc works ##########
+pred = (pred.boost$prob)[,1]
+dummytest <- dummy.data.frame(cmDatatraining[-train,])
+y = dummytest$classfarm
+roc1 = roc(y, pred)
+plot(roc1, col = 1, lty = 2, main = "ROC for Farm Class")
+
+
+(45+72+22+37+42+49)/301
+(39+57+7+37+9+21)/301
+
+(287+1853+91+217+38)/(.25*nrow(cmDatatraining))
+
+
+train.gbm <- train(as.factor(cmDatatraining[train,]$class)~., data=data.train ,method="gbm",verbose=F)
+?head
